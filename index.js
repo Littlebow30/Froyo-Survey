66a3c3af20bfacaaca22141f72be8feb0a06b5c6
Littlebow30 <bowmanA1906@gmail.com>
2023-09-04 22:09:14 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// You notice that Jake made some mistakes when filling out the survey. You talk to Jake and gather the correct information. Modify the following properties in the customer object using bracket notation: 
+// email to "Jak3Smith1992@email.com"
+// phone to 3195551234
+// zipCode to "63132"
+// favoriteFlavors to "coffee", "strawberry", and "matcha"
